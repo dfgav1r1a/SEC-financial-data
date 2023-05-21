3b15f21957a5b1beb5a9488421e42bbed5c14b92
dfgaviria <dfgaviria.dev@gmail.com>
2023-05-20 22:51:34 -0400

    filtered 10-K forms successful, showing the json in  the page

--- public/modules/domElements.module.js
+++ public/modules/domElements.module.js
@@ -1,34 +1,8 @@
 //IDBData is coming from the fn 'getTickersRequest' that executes GET req to IndexedDB's
 //"commpanyTickers" obj store
 
-// const datalistFiller = (IDBData) => {
-//     const dataDisplay = document.querySelector('#dataDisplay');
-
-//     //cleaning up DOM tree for next data set
-//     const previousUlElement = document.querySelector('ul');
-//     const previousPElement = document.querySelector('p');
-//     if (previousPElement) previousPElement.remove();
-
-//      //To show msg error in page if IDBData is null/undefined
-//     if(!IDBData) {
-//         const pEle = document.createElement('p');
-//         pEle.textContent = "Uh-Oh it seems the ticker symbol is incorrect, please check and try again";
-//         dataDisplay.appendChild(pEle);
-//     }
-
-//     //adding next data set
-//     const ulELe = document.createElement('ul');
-
-//     for (const property in IDBData) {
-//         const liEle = document.createElement('li');
-//         liEle.textContent = `${property} : ${IDBData[property]}`; 
-//         ulELe.appendChild(liEle);
-//     }
-
-//     dataDisplay.appendChild(ulELe);
-// }
-
 const datalistFiller = (SECData) => {
+    //SECData is coming as a string from fetchToApi module
     const dataDisplay = document.querySelector('#dataDisplay');
 
     //cleaning up DOM tree for next data set
@@ -47,12 +21,6 @@ const datalistFiller = (SECData) => {
     //adding next data set
     const preELe = document.createElement('pre');
     preELe.textContent = SECData;
-
-    // for (const property in IDBData) {
-    //     const liEle = document.createElement('li');
-    //     liEle.textContent = `${property} : ${IDBData[property]}`; 
-    //     ulELe.appendChild(liEle);
-    // }
 
     dataDisplay.appendChild(preELe);
 }
--- public/modules/fetchToAPI.module.js
+++ public/modules/fetchToAPI.module.js
@@ -4,11 +4,8 @@ const fetchToSEC = async IDBRecord => {
     //converting the cik number from IDB to a string and then adding zeros at the start of string to accurately generate the CIK for the SEC URL
     const CIKNumber = IDBRecord.cik_str.toString().padStart(10, "0");
     console.log("CIK requested:", CIKNumber);
-    // console.log(CIKNumber.length)
 
     try {
-        // const response = await fetch(`https://data.sec.gov/api/xbrl/companyconcept/CIK${CIKNumber}/us-gaap/AccountsPayableCurrent.json`,
-        // const response = await fetch(`https://www.sec.gov/files/company_tickers.json`,
         const response = await fetch(`https://data.sec.gov/submissions/CIK${CIKNumber}.json`,
     {
         headers: {
@@ -18,13 +15,50 @@ const fetchToSEC = async IDBRecord => {
         },
     });
     const data = await response.json();
-    console.log(data);
-    const prettyData = JSON.stringify(data, null, 2);
+    //next, filtering 10k forms, fn in line 32
+    const listOf10K = filtered10kData(data);
+    const prettyData = JSON.stringify(listOf10K, null, 2);
     //next, call the helper fn from domElements.module.js to render data in browser
     datalistFiller(prettyData);
     } catch (error) {
         console.error(`Pulling data from the SEC was not successful: ${error.message}`)
     }
+}
+
+const filtered10kData = ({filings}) => {
+    const formsArray = filings.recent.form;
+    const existent10KIndexes = []; 
+    const filtered10kDataArray = [];
+    let form = null;
+    let filingDate = null;
+    let reportDate = null;
+    let primaryDocument = null;
+    let accessionNumber = null;
+    //looping through formsArray to get the indexes of existent 10-k records
+    for(let i = 0; i < formsArray.length; i ++) {
+       if (formsArray[i] === '10-K') existent10KIndexes.push(i); 
+    }
+    console.log('10-K are in indexes', existent10KIndexes);
+   //looping through existent10KIndexes to get related data to the indexes saved 
+    for(let index of existent10KIndexes) {
+        form = filings.recent.form[index];
+        filingDate = filings.recent.filingDate[index]; 
+        reportDate = filings.recent.reportDate[index]; 
+        primaryDocument = filings.recent.primaryDocument[index]; 
+        accessionNumber = filings.recent.accessionNumber[index]; 
+        filtered10kDataArray.push(new filtered10kObject(form, filingDate, reportDate, primaryDocument, accessionNumber));
+    }
+    //creating the object with the 10-K formdata. Every single item will correspond to the indexes found in line 43 
+    function filtered10kObject(form, filingDate, reportDate, primaryDocument, accessionNumber) {
+            this.form = form,
+            this.filingDate = filingDate,
+            this.reportDate = reportDate,
+            this.primaryDocument = primaryDocument,
+            this.reportDate = reportDate,
+            this.accessionNumber = accessionNumber
+    }
+    console.log('filtered 10kforms are', filtered10kDataArray);
+    return filtered10kDataArray
 }
 
 export {
